b296db036ccc60e104be72a79e9cf42740733e01
sabrina170 <sabrina.pomajulca@tecsup.edu.pe>
2019-03-21 12:11:30 -0500

    SabrinaPomajulca

---
+++ src/main/java/com/tecsup/grupo7/Lab_Calif_01_Git_Maven/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.grupo7.Lab_Calif_01_Git_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
